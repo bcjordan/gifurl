eb90c9b844bf9a65b63f7b7fd290771f0fa0489e
Brian Jordan <bcjordan@gmail.com>
2011-07-31 00:16:02 -0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllgifsCom::Application.config.secret_token = '29e73f420dce1a7fe6d94c24d4031032bba12b3fd280bccd32450101beb3e2281df277cdb181a2aa05ae38ae2c121a6881fd1abfc1b892ed60fe5383606251c0'
